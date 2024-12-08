e80014d988f5caa847daee50cb0c8f4771c4646b
Shunsuke Kamada <kamada@sansan.com>
2024-12-09 01:29:49 +0900

    Initial Commit

---
+++ Mahjong-seiseki-sender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mahjong-seiseki-sender
+//
+//  Created by Shunsuke Kamada on 2024/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mahjong-seiseki-sender/Mahjong_seiseki_senderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mahjong_seiseki_senderApp.swift
+//  Mahjong-seiseki-sender
+//
+//  Created by Shunsuke Kamada on 2024/12/09.
+//
+
+import SwiftUI
+
+@main
+struct Mahjong_seiseki_senderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
