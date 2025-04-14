f550c42bcaf524f8fa44960e81e02b99a962e295
Shunsuke Kamada <kamada@sansan.com>
2025-04-14 19:30:59 +0900

    リファクタとビルドエラー解消
    ルール名の省略形を表示するように
    プラスは+、マイナスは▲をつけて表示するように
    コンパイルエラーに対応

--- Mahjong-seiseki-sender/ContentView.swift
+++ Mahjong-seiseki-sender/ContentView.swift
@@ -15,92 +15,126 @@ struct ContentView: View {
     @State var point: Float = 0.0
     @State var rule = Rule.M_REAGUE
     @State var results: [GameResult] = [
-        GameResult(timeStamp: Date(), description: "1戦目", point: 33.4, rank: Rank.first, rule: Rule.KYOKAI),
-        GameResult(timeStamp: Date(), description: "2戦目", point: 33.4, rank: Rank.second, rule: Rule.SAIKOUISEN),
-        GameResult(timeStamp: Date(), description: "3戦目", point: -33.4, rank: Rank.first, rule: Rule.RENMEI)
+        GameResult(timeStamp: Date(), description: "1戦目", point: Point(value: 33.4), rank: Rank.first, rule: Rule.KYOKAI),
+        GameResult(timeStamp: Date(), description: "2戦目", point: Point(value: 33.4), rank: Rank.second, rule: Rule.SAIKOUISEN),
+        GameResult(timeStamp: Date(), description: "3戦目", point: Point(value: -33.4), rank: Rank.third, rule: Rule.RENMEI)
     ]
 
     @State var isSending = false
 
     var body: some View {
         VStack {
-            HStack {
-                Text("description")
-                Text("*").foregroundColor(.red)
-                TextField("例: ベルバードリーグ", text: $description).frame(maxWidth: .infinity, alignment: .trailing).multilineTextAlignment(.trailing)
-            }
-
-            HStack {
-                Text("point")
-                Text("*").foregroundColor(.red)
-                TextField("5桁の数字 例: 12300", value: $point, format: .number).frame(maxWidth: .infinity, alignment: .trailing).multilineTextAlignment(.trailing).keyboardType(.decimalPad)
-            }
-
-            // TODO: スペース管理、幅をもう少し小さくしたい
-            HStack {
-                Text("rank")
-                Text("*").foregroundColor(.red)
-                Spacer()
-                Picker("順位", selection: $rank) {
-                    ForEach(Rank.allCases) {
-                        Text("\($0.rawValue)").tag($0)
-                    }
-                }.pickerStyle(.segmented)
-            }
-
-            HStack {
-                Text("rule")
-                Text("*").foregroundColor(.red)
-                Picker("", selection: $rule) {
-                    ForEach(Rule.allCases) {
-                        Text("\($0.rawValue)").tag($0)
-                    }
-                }.pickerStyle(.wheel)
-            }
-
-            // TODO: ボタンを押したらレスポンスに合わせてOK/NGなUIを出す (コンタクト登録時に出るアレみたいな)
-            // TODO: validate
-            HStack {
-                Button(action: {
-                    isSending = true
-                    ApiClient.shared.saveResults(
-                        gameResult: GameResult(
-                            timeStamp: Date(),
-                            description: description,
-                            point: point,
-                            rank: rank,
-                            rule: rule
-                        )
-                    ) { _ in
-                        isSending = false
-                    }
-                }, label: {
-                    Text("Send")
-                })
-
-                if (isSending) {
-                    ProgressView().progressViewStyle(.circular)
-                }
-            }
-            
-            List {
-                ForEach(results) { result in
-                    HStack {
-                        Text(result.timeStamp.format())
-                        Text(result.rule.rawValue)
-                        Spacer()
-                        Text(result.rank.toString())
-                        Text("\(result.point)")
-                    }
-                }
-            }
-            .listStyle(.plain)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color(red: 200/255, green: 200/255, blue: 200/255), lineWidth: 2)
-            )
+            inputSection
+            resultListSection
         }
         .padding()
+    }
+    
+    /// 入力欄
+    private var inputSection: some View {
+        VStack {
+            descriptionInput
+            pointInput
+            rankInput
+            ruleInput
+            sendButton
+        }
+    }
+    
+    /// 説明欄
+    private var descriptionInput: some View {
+        HStack {
+            Text("description")
+            Text("*").foregroundColor(.red)
+            TextField("例: ベルバードリーグ", text: $description)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .multilineTextAlignment(.trailing)
+        }
+    }
+    
+    /// ポイントの入力欄
+    private var pointInput: some View {
+        HStack {
+            Text("point")
+            Text("*").foregroundColor(.red)
+            TextField("5桁の数字 例: 12300", value: $point, format: .number)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .multilineTextAlignment(.trailing)
+                .keyboardType(.decimalPad)
+        }
+    }
+    
+    /// 順位の入力欄
+    private var rankInput: some View {
+        HStack {
+            Text("rank")
+            Text("*").foregroundColor(.red)
+            Spacer()
+            Picker("順位", selection: $rank) {
+                ForEach(Rank.allCases) {
+                    Text("\($0.rawValue)").tag($0)
+                }
+            }.pickerStyle(.segmented)
+        }
+    }
+    
+    /// ルールの入力欄(ドラム)
+    private var ruleInput: some View {
+        HStack {
+            Text("rule")
+            Text("*").foregroundColor(.red)
+            Picker("", selection: $rule) {
+                ForEach(Rule.allCases) {
+                    Text("\($0.rawValue)").tag($0)
+                }
+            }.pickerStyle(.wheel)
+        }
+    }
+    
+    /// 送信ボタン
+    // TODO: 送信時の処理をUsecase的なところに移植する
+    private var sendButton: some View {
+        HStack {
+            Button(action: {
+                isSending = true
+                ApiClient.shared.saveResults(
+                    gameResult: GameResult(
+                        timeStamp: Date(),
+                        description: description,
+                        point: Point(value: point),
+                        rank: rank,
+                        rule: rule
+                    )
+                ) { _ in
+                    isSending = false
+                }
+            }, label: {
+                Text("Send")
+            })
+
+            if (isSending) {
+                ProgressView().progressViewStyle(.circular)
+            }
+        }
+    }
+    
+    private var resultListSection: some View {
+        List {
+            ForEach(results, id: \.timeStamp) { result in
+                HStack {
+                    Text(result.timeStamp.format())
+                    Text(result.rule.shortHanded)
+                    Spacer()
+                    Text(result.rank.toString())
+                    Text(result.point.value > 0 ? "+" + String(format: "%.1f", round(result.point.value * 10) / 10) : String(format: "%.1f", round(result.point.value * 10) / 10))
+                }
+            }
+        }
+        .listStyle(.plain)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(red: 200/255, green: 200/255, blue: 200/255), lineWidth: 2)
+        )
     }
 }
 
--- Mahjong-seiseki-sender/GameResult.swift
+++ Mahjong-seiseki-sender/GameResult.swift
@@ -8,21 +8,39 @@
 import Foundation
 
 struct GameResult: APIParameterConvertible, Identifiable {
-    var id: String { "\(timeStamp) \(description)" }
-    
-    var timeStamp: Date
-    var description: String
-    var point: Float
-    var rank: Rank
-    var rule: Rule
+    let timeStamp: Date
+    let description: String
+    let point: Point
+    let rank: Rank
+    let rule: Rule
 
+    var id: Date { timeStamp }
+
+    /// APIで送信するためのdictionary
     func toDictionary() -> [String : Any] {
         return [
-            "description": description,
-            "point": point,
+            // timestampとdesctiptionがスペースが入ってjsonエンコードに失敗する可能性があるので""でくくっておく
+            "timeStamp": "\(timeStamp)",
+            "description": "\(description)",
+            "point": point.value,
             "rank": rank.rawValue,
             "rule": rule.rawValue
         ]
+    }
+}
+
+// プラスの場合+、マイナスの場合▲の記号をつけるようにする
+struct Point {
+    let value: Float
+    var signedString: String {
+        let formatter = NumberFormatter()
+        formatter.positivePrefix = "+"
+        formatter.negativePrefix = "▲"
+        return formatter.string(from: NSNumber(value: value)) ?? "\(value)"
+    }
+    
+    init(value: Float) {
+        self.value = value
     }
 }
 
